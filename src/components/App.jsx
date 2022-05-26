042b7adb16408dd740a9b10a465d4bb3e0af51b8
ed_bolma <ed.bolma21@gmail.com>
2022-05-26 09:19:34 +0200

    Task is completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,30 @@ import { ContactList } from './ContactList/ContactList';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
+
+    // [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
     filter: '',
   };
+
+  componentDidMount() {
+    console.log('didMount worked');
+
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (this.state.contacts) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
+
+  componentDidUpdate() {
+    console.log('didUpdate worked');
+
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
 
   deleteContact = contactId => {
     this.setState(prevState => ({
